e34e4d67ae5b699600e44e12f58f79d6a3c28d77
yukselsule <sulecantekin@gmail.com>
2024-05-16 17:39:07 +0300

    done

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -2,6 +2,16 @@ import icons from '../../img/icons.svg';
 
 export default class View {
   _data;
+
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered {e.g. recipe}
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render is false
+   * @this {Object} View instance
+   * @author Şule Cantekin
+   * @todo Finish implemantation
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
